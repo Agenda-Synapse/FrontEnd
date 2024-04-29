766c89b11dba5dcb9d8abf4712a5791a47b2af6c
Lucasnogsilva <lucastopg00@gmail.com>
2024-04-29 10:20:23 -0300

    fix: posistion of elements

--- src/app/pages/perfil-shop/perfil-shop.component.ts
+++ src/app/pages/perfil-shop/perfil-shop.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-perfil-shop',
   templateUrl: './perfil-shop.component.html',
-  styleUrl: './perfil-shop.component.scss'
+  styleUrls: ['./perfil-shop.component.scss']
 })
 export class PerfilShopComponent {
 
