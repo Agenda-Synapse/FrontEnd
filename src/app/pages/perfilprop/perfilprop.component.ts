4210dfd6ec7697c7d7f8dab9d1ec4716730d2836
Daniel <danieldodesign2005@gmail.com>
2024-05-29 10:41:34 -0300

    criacao pagagina perfil dos propietarios

---
+++ src/app/pages/perfilprop/perfilprop.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-perfilprop',
+  templateUrl: './perfilprop.component.html',
+  styleUrls: ['./perfilprop.component.scss']
+})
+export class PerfilpropComponent {
+
+}
