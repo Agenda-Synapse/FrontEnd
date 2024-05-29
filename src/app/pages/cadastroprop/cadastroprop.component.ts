caa71d1d56d3a0486586b2ac76f598fba2f026be
Lucasnogsilva <lucastopg00@gmail.com>
2024-05-29 08:05:27 -0300

    Fix: error imagem

--- src/app/pages/cadastroprop/cadastroprop.component.ts
+++ src/app/pages/cadastroprop/cadastroprop.component.ts
@@ -1,10 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  FormBuilder,
-  FormGroup,
-  Validators,
-  AbstractControl,
-} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -20,21 +15,19 @@ export class CadastropropComponent implements OnInit {
   step2Form!: FormGroup;
   step3Form!: FormGroup;
   showError: boolean = false;
+  showImageError: boolean = false; // Variável de controle de erro para imagem
   isImageSelected: boolean = false;
   isFormTouched: boolean = false;
 
   constructor(private fb: FormBuilder, private router: Router) {}
 
   ngOnInit(): void {
-    this.step1Form = this.fb.group(
-      {
-        nome: ['', Validators.required],
-        email: ['', [Validators.required, Validators.email]],
-        senha: ['', Validators.required],
-        confirmarSenha: ['', Validators.required],
-      },
-      { validator: this.passwordMatchValidator }
-    );
+    this.step1Form = this.fb.group({
+      nome: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', Validators.required],
+      confirmarSenha: ['', Validators.required],
+    }, { validator: this.passwordMatchValidator });
 
     this.step2Form = this.fb.group({
       nomeEstabelecimento: ['', Validators.required],
@@ -51,9 +44,7 @@ export class CadastropropComponent implements OnInit {
     });
   }
 
-  passwordMatchValidator(
-    control: AbstractControl
-  ): { [key: string]: boolean } | null {
+  passwordMatchValidator(control: AbstractControl): { [key: string]: boolean } | null {
     const senha = control.get('senha');
     const confirmarSenha = control.get('confirmarSenha');
     if (senha && confirmarSenha && senha.value !== confirmarSenha.value) {
@@ -78,6 +69,7 @@ export class CadastropropComponent implements OnInit {
     const input = event.target as HTMLInputElement;
     if (input.files && input.files.length > 0) {
       this.isImageSelected = true;
+      this.showImageError = false; // Resetar o erro quando a imagem for selecionada
       this.onFileChange(event);
     }
   }
@@ -87,8 +79,7 @@ export class CadastropropComponent implements OnInit {
     const reader = new FileReader();
     reader.onloadend = () => {
       const base64String = reader.result as string;
-      // console.log(base64String);
-      return this.imagEmBase64 = base64String;
+      this.imagEmBase64 = base64String;
     };
     if (file) {
       reader.readAsDataURL(file);
@@ -103,18 +94,31 @@ export class CadastropropComponent implements OnInit {
   next() {
     this.showError = false;
     this.isFormTouched = true;
-    if (this.currentContainer === 1 && this.isStep1Complete()) {
-      console.log('primeira parte do form', this.step1Form.value);
-      // debugger
-      this.currentContainer = 2;
-    } else if (this.currentContainer === 2 && this.isStep2Complete()) {
-      console.log('Segunda parte do form', this.step2Form.value);
-      this.currentContainer = 3;
-    } else if (this.currentContainer === 3 && this.isStep3Complete()) {
-      // Avançar para a próxima etapa ou finalizar
-    } else {
-      this.showError = true;
-      this.validateAllFormFields(this.getCurrentForm());
+
+    if (this.currentContainer === 1) {
+      if (this.isStep1Complete()) {
+        console.log('primeira parte do form', this.step1Form.value);
+        this.currentContainer = 2;
+      } else {
+        this.showError = true;
+        this.validateAllFormFields(this.step1Form);
+      }
+    } else if (this.currentContainer === 2) {
+      this.showImageError = !this.isImageSelected; // Mostrar erro se a imagem não for selecionada
+      if (this.isStep2Complete()) {
+        console.log('Segunda parte do form', this.step2Form.value);
+        this.currentContainer = 3;
+      } else {
+        this.showError = true;
+        this.validateAllFormFields(this.step2Form);
+      }
+    } else if (this.currentContainer === 3) {
+      if (this.isStep3Complete()) {
+        // Avançar para a próxima etapa ou finalizar
+      } else {
+        this.showError = true;
+        this.validateAllFormFields(this.step3Form);
+      }
     }
   }
 
