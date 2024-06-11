3677fe1b0a22e61a8cd5dcdfe63f60f6c03b7f54
Lucasnogsilva <lucastopg00@gmail.com>
2024-06-11 08:17:57 -0300

    started: função botão para adicionar fotos

--- src/app/pages/perfilprop/perfilprop.component.ts
+++ src/app/pages/perfilprop/perfilprop.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./perfilprop.component.scss']
 })
 export class PerfilpropComponent {
-
+  services = [
+    { name: 'Corte degrade', time: '01:00', price: '30,00',  },
+    { name: 'Barba', time: '00:45', price: '25,00' },
+    { name: 'Corte  Degrade', time: '01:30', price: '30,00' },
+    { name: 'Barba', time: '00:45', price: '25,00' }
+  ];
 }
